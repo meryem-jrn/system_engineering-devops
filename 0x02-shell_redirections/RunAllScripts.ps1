f5ee7e2fd3ce27d1b022b3e0ea5c16114bd406a3
Meryem Jirani <meryemjirani9@gmail.com>
2025-08-13 15:28:10 +0200

    Completed all 0x02-shell_redirections tasks

---
+++ 0x02-shell_redirections/RunAllScripts.ps1
@@ -0,0 +1,22 @@
+Write-Output '--- Running 0-hello_world ---'
+.\0-hello_world
+Write-Output '--- Running 1-confused_smiley ---'
+.\1-confused_smiley
+Write-Output '--- Running 2-hellofile ---'
+.\2-hellofile
+Write-Output '--- Running 3-twofiles ---'
+.\3-twofiles
+Write-Output '--- Running 4-lastlines ---'
+.\4-lastlines
+Write-Output '--- Running 5-firstlines ---'
+.\5-firstlines
+Write-Output '--- Running 6-third_line ---'
+.\6-third_line
+Write-Output '--- Running 7-file ---'
+.\7-file
+Write-Output '--- Running 8-cwd_state ---'
+.\8-cwd_state
+Write-Output '--- Running 9-duplicate_last_line ---'
+.\9-duplicate_last_line
+Write-Output '--- Running 10-no_more_js ---'
+.\10-no_more_js
